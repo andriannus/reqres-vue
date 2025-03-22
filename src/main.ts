05becca124c00562517622f6fdf13e37ceaad8ed
Andriannus Parasian <andriannus.parasian@efishery.com>
2025-03-22 11:53:52 +0700

    feat: setup Vue Query

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import { createApp } from 'vue';
 
 import App from './App.vue';
 import { router } from './router';
 
-createApp(App).use(router).mount('#app');
+const app = createApp(App);
+
+app.use(router);
+
+app.use(VueQueryPlugin, {
+  queryClientConfig: {
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  },
+});
+
+app.mount('#app');
